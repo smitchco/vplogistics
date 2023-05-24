dcff0317333efd8d46f1cd5f3d3385fa50c9f9d8
Matt Smith <matt@smitch.co>
2023-05-23 22:10:07 -0500

    child template

---
+++ web/app/themes/vplogistics/template-child.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * Template Name: Child Template
+ *
+ * Template for child pages
+ */
+
+get_header(); ?>
+
+<main>
+  <section class="pt-5">
+    <div class="container-fluid container--max">
+      <div class="row justify-content-center align-items-center">
+        <div class="col-12 col-lg-6 py-md-5">
+          <h1 class="header__title text-uppercase lh-100 py-md-5">
+            <?php echo get_field('hero_text'); ?>
+          </h1>
+        </div>
+        <div class="col-12 col-lg-6">
+          <div class="p-5">
+
+            OUR SERVICES
+            
+            <?php the_content(); ?>
+
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+
+
+
+<main class="main" role="main">
+  <div class="container-fluid container--max">
+    <section>
+    </section>
+  </div>
+</main>
+
+<?php get_footer(); ?>
